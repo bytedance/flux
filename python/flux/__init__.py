7f98c8ab847449983a46810f8a9b7b53484935d0
Ningxin Zheng <49771382+zheng-ningxin@users.noreply.github.com>
2024-10-16 09:43:33 +0800

    Update __init__.py (#45)
    update version to 1.0.4.
    Will support torch2.5 in this version

--- python/flux/__init__.py
+++ python/flux/__init__.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 ################################################################################
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 
 from .cpp_mod import *
 from .ag_gemm import *
