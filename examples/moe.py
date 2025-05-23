e6f6b3b06b95f9b4f0793d69c9daa3f3c86579f9
burness <burness1990@gmail.com>
2025-05-23 08:08:05 +0000

    revert mlp_output

--- examples/moe.py
+++ examples/moe.py
@@ -167,10 +167,10 @@ class MoE_layer_flux(torch.nn.Module):
 
         # MLP layer 1 (GEMM1 and combine)
         mlp_output = self.flux_rs_op.forward_gather_rs(
-            self.ctx.intermediate_output,
-            self.ctx.weight1,
-            self.ctx.splits_cpu,
-            self.ctx.scatter_index.view(-1),
+            input=self.ctx.intermediate_output,
+            weight=self.ctx.weight1,
+            splits_cpu=self.ctx.splits_cpu,
+            rounting_idx=self.ctx.scatter_index.view(-1),
         )
 
         return mlp_output
